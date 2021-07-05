07dc94a4925cdb9c2447ac009fe7854ba4555fec
Aayush <joshiaayush871@gmail.com>
2021-07-05 19:48:25 +0545

    Initial commit

---
+++ OPP/CLASSES.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name, age, height):
+        self.name = name
+        self.age = age
+        self.height = height
+
+
+p1 = Person("Aayush Raj Joshi", 21,"5.4 inch")
+print(p1.name)
+print(p1.age)
+print(p1.height)
